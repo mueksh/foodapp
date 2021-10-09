58d522a0c24e62e5c6fa68786bbb02a988fcdef6
mukesh <72579414+mueksh@users.noreply.github.com>
2021-10-09 11:29:38 +0530

    Update FoodRepository.java

--- src/main/java/com/mukesh/food/app/repo/FoodRepository.java
+++ src/main/java/com/mukesh/food/app/repo/FoodRepository.java
@@ -1,13 +1,12 @@
 package com.mukesh.food.app.repo;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mukesh.food.app.entities.Food;
 
 @Repository
-public interface FoodRepository extends JpaRepository<Food, Integer>{
+public interface FoodRepository extends MongoRepository<Food, Integer>{
 
 }
